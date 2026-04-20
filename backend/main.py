a62315038aae6577e4405f7bf5bcc9400385488b
Nick Fallon <144065117+Veddersboy@users.noreply.github.com>
2026-04-19 23:01:45 -0400

    Fix CORS origin URL formatting

--- backend/main.py
+++ backend/main.py
@@ -42,7 +42,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://openmatch-frontend-osdn.onrender.com/",
+        "https://openmatch-frontend-osdn.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
